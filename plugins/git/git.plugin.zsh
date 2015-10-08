aca6b7ffa98007d672d0cf7f7bceaa028af82613
Gunnar Bastkowski <gunnar@bastkowski.name>
2015-10-08 12:00:08 +0200

    Add alias for git svn fetch

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -228,6 +228,7 @@ alias grv='git remote -v'
 
 alias gsb='git status -sb'
 alias gsd='git svn dcommit'
+alias gsf='git svn fetch'
 alias gsi='git submodule init'
 alias gsps='git show --pretty=short --show-signature'
 alias gsr='git svn rebase'
